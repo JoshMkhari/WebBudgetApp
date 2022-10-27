9da6106e53856a98dd76758b3061b418e4232772
JoshMkhari <njmkhari@gmail.com>
2022-10-27 15:17:41 +0200

    Bryan figured out styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,18 +9,6 @@
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="resources/views/layouts/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="resources/views/layouts/assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="resources/views/layouts/assets/css/style.css" rel="stylesheet">
-
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -1169,16 +1157,4 @@
     </div>
 </footer><!-- End Footer -->
 
-<!-- Vendor JS Files -->
-<script src="resources/views/layouts/assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/chart.js/chart.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/echarts/echarts.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/quill/quill.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="resources/views/layouts/assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="resources/views/layouts/assets/vendor/php-email-form/validate.js"></script>
-
-<!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,92 +13,34 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/simpledatabasestyle.css') }}" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @yield('content')
     </div>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script type="text/javascript" src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/chart.js/chart.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/echarts/echarts.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/quill/quill.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script type="text/javascript" src="assets/vendor/tinymce/tinymce.min.js"></script>
-    <script type="text/javascript" src="assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
-    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/chart.min.js') }}"></script>
+    <script src="{{ asset('js/echarts.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="{{ asset('js/quill.min.js') }}"></script>
+    <script src="{{ asset('js/simple-datatables.js') }}"></script>
+    <script src="{{ asset('js/validate.js') }}"></script>
 
 </body>
 </html>
