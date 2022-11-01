7ee7828bd8f948230eea66d18d3ed0ad1d50cee1
JoshMkhari <njmkhari@gmail.com>
2022-11-01 09:54:04 +0200

    Changed minimum for validator Role from 1 to 0

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'integer', 'min:1','max:3'],
+            'role' => ['required', 'integer', 'min:0','max:3'],
         ]);
     }
 
